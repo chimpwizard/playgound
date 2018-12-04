74f0b5e5846d05b0baf799f782568b0d73b07829
ndruwizz <ndru@chimpwizard.com>
2018-12-04 17:54:10 -0500

    more about pi -- the nodejs sample is working now

--- iot/pi/docker/node/app.2.js
+++
@@ -1,5 +0,0 @@
-//
-// https://www.npmjs.com/package/pi-pins
-//
-var pin1 = require("pi-pins").connect(18);
-pin1.mode('high'); 
--- iot/pi/docker/node/app.3.js
+++
@@ -1,3 +0,0 @@
-//
-// https://www.w3schools.com/nodejs/nodejs_raspberrypi_blinking_led.asp
-//
