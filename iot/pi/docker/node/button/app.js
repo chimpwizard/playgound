be0be44fe076329dc3883ee339c55e00f0563575
ndruwizz <ndru@chimpwizard.com>
2018-12-08 09:28:59 -0500

    update code

--- iot/pi/docker/node/button/app.js
+++ iot/pi/docker/node/button/app.js
@@ -2,4 +2,5 @@ const Gpio = require('onoff').Gpio;
 const led = new Gpio(18, 'out');
 const button = new Gpio(26, 'in', 'both');
  
+console.log("Listening port 26...")
 button.watch((err, value) => led.writeSync(value));
